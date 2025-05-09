2069784e29de44b51b55d4938c9e8c30dc710ad4
People7 <nd07101970@gmail.com>
2025-05-09 15:50:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,29 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 // Updated Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDS6oRCGu8BW3fXnpp6L4AhkNJ4OsGpwtk",
-  authDomain: "skillbook-3fa41.firebaseapp.com",
-  projectId: "skillbook-3fa41",
-  storageBucket: "skillbook-3fa41.appspot.com",  // Corrected bucket name
-  messagingSenderId: "599549421075",
-  appId: "1:599549421075:web:9092eeb6bc3765baebc1e6",
-  measurementId: "G-3VKK01HXE6"
-};
+    apiKey: "AIzaSyDS6oRCGu8BW3fXnpp6L4AhkNJ4OsGpwtk",
+    authDomain: "skillbook-3fa41.firebaseapp.com",
+    projectId: "skillbook-3fa41",
+    storageBucket: "skillbook-3fa41.firebasestorage.app",
+    messagingSenderId: "599549421075",
+    appId: "1:599549421075:web:9092eeb6bc3765baebc1e6",
+    measurementId: "G-3VKK01HXE6"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const messaging = getMessaging(app);
 const db = getFirestore(app);
+</script>
 
 export { app, auth, messaging, db };
 
